f31babad73c91e1263b7be9c01ec797b125f06b2
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2026-02-25 14:15:54 +0000

    Create the play round function

--- js/main.js
+++ js/main.js
@@ -7,21 +7,42 @@ function getComputerChoice() {
 
     switch (randomNumber) {
     case 1:
-        console.log('rock');
-        break;
+        return 'rock';
+    
     case 2:
-        console.log('paper');
-        break;
+        return 'paper';
+
     case 3:
-        console.log('scissors');
-        break;
+        return 'scissors';
     
     }
 }
 
 function getHumanChoice() {
     let humanChoice = prompt('Enter either rock, paper or scissors')
-    console.log(humanChoice);
+    return humanChoice;
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+  
+    if (humanChoice === 'rock' && computerChoice === 'scissors'){
+        humanScore++;
+        console.log(`You win!! \n  score is Player: ${humanScore} \n Computer: ${computerScore} `);
+    } else if (humanChoice === 'paper' && computerChoice === 'rock'){
+        humanScore++;
+        console.log(`You win!! \n  score is Player: ${humanScore} \n Computer: ${computerScore} `);
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+        humanScore++;
+        console.log(`You win!! \n  score is Player: ${humanScore} \n Computer: ${computerScore} `);
+    } else if (humanChoice === computerChoice){
+        console.log(`You draw \n Player: ${humanChoice} \n Computer: ${computerChoice}`);
+    } else {
+        computerScore++;
+        console.log(`You lose ${computerChoice} beats ${humanChoice} \n Player: ${humanScore} \n Computer: ${computerScore}`);
+    }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
