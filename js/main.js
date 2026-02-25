461528c7d218bdc19ef46bdd5a691f7091f9ac76
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2026-02-25 12:25:37 +0000

    Create the getComputerChoice function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch (randomNumber) {
+    case 1:
+        console.log('rock');
+        break;
+    case 2:
+        console.log('paper');
+        break;
+    case 3:
+        console.log('scissors');
+        break;
+    
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt('Enter either rock, paper or scissors')
+    console.log(humanChoice);
+}
+
+getHumanChoice();
