afc7a7f2a63224efbb792f8271a18f1b116d17f7
uyanda-maseti <masetibabalo2@gmail.com>
2026-05-12 03:50:08 +0200

    create a while loop that runs fives times calling the playround function each time it runs

--- js/main.js
+++ js/main.js
@@ -1,60 +1,75 @@
-
-const humanSelection = getHumanChoice().toLowerCase();
-const computerSelection = getComputerChoice();
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-      
-        if (humanChoice === 'rock' && computerChoice === 'scissors'){
-            humanScore++;
-            console.log(`You win!! \n  score is Player: ${humanScore} \n Computer: ${computerScore} `);
-        } else if (humanChoice === 'paper' && computerChoice === 'rock'){
-            humanScore++;
-            console.log(`You win!! \n  score is Player: ${humanScore} \n Computer: ${computerScore} `);
-        } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
-            humanScore++;
-            console.log(`You win!! \n  score is Player: ${humanScore} \n Computer: ${computerScore} `);
-        } else if (humanChoice === computerChoice){
-            console.log(`You draw \n Player: ${humanChoice} \n Computer: ${computerChoice}`);
-        } else {
-            computerScore++;
-            console.log(`You lose ${computerChoice} beats ${humanChoice} \n Player: ${humanScore} \n Computer: ${computerScore}`);
-        }
-    }
-    
-    
-    for (let i = 0; i < 5; i++){
-        playRound(humanSelection, computerSelection);
-    }
-}
-
-
-
-function getComputerChoice() {
-    
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-
-    switch (randomNumber) {
-    case 1:
-        return 'rock';
-    
-    case 2:
-        return 'paper';
-
-    case 3:
-        return 'scissors';
-    
-    }
-}
-
 function getHumanChoice() {
     let humanChoice = prompt('Enter either rock, paper or scissors')
     return humanChoice;
 }
 
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch (randomNumber) {
+        case 1:
+            return 'rock'
+
+        case 2:
+            return 'paper'
+
+        case 3:
+            return 'scissors'
+
+    }
+}
 
 
-playGame();
+
+
+function playGame() {
+
+    let humanScore = 0
+    let computerScore = 0
+    let play = true
+
+    function playRound(humanChoice, computerChoice) {
+
+        if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            humanScore++
+            console.log(`You win!!! \nPlayer score: ${humanScore} \n Computer score: ${computerScore} `);
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            humanScore++
+            console.log(`You win!!! \nPlayer score: ${humanScore} \nComputer score: ${computerScore} `);
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            humanScore++
+            console.log(`You win!!! \nPlayer score: ${humanScore} \nComputer score: ${computerScore} `);
+        } else if (humanChoice === computerChoice) {
+            console.log(`You draw \nPlayer score: ${humanChoice} \nComputer score: ${computerChoice}`);
+        } else {
+            computerScore++
+            console.log(`You lose ${computerChoice} beats ${humanChoice} \nPlayer score: ${humanScore} \nComputer score : ${computerScore}`);
+        }
+    }
+
+    function playAgain(){
+        let userInput = prompt("Enter Y/N:")
+        userInput.toLowerCase()
+        if (userInput == 'y'){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    let round = 0;
+
+    while (playAgain && round < 5) {
+        const humanSelection = getHumanChoice().toLowerCase()
+        const computerSelection = getComputerChoice()
+
+        playRound(humanSelection, computerSelection)
+        play = playAgain()
+        round++
+    }
+
+
+}
+
+playGame()
